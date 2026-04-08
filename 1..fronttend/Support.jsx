c40f70bb92e6445cae4b2d2f9be444799ac6dafc
kalkikalki373-ux <kalkikalki373@gmail.com>
2026-04-08 15:42:07 +0530

    Create Support.jsx

---
+++ 1..fronttend/Support.jsx
@@ -0,0 +1,9 @@
+export default function Support() {
+  return (
+    <div className="container">
+      <h1>Customer Support</h1>
+      <textarea placeholder="Enter customer issue..." rows="5"></textarea>
+      <button>Submit Ticket</button>
+    </div>
+  );
+}
