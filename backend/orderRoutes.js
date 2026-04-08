6ed5741eb7bb6674e19bb74c39c7bd4893258750
kalkikalki373-ux <kalkikalki373@gmail.com>
2026-04-08 16:23:33 +0530

    Create orderRoutes.js

---
+++ backend/orderRoutes.js
@@ -0,0 +1,10 @@
+import express from "express";
+import { createOrder, getOrders, updateOrder } from "../controllers/orderController.js";
+
+const router = express.Router();
+
+router.post("/", createOrder);
+router.get("/", getOrders);
+router.put("/:id", updateOrder);
+
+export default router;
